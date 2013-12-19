15869eb7ea4359c5d045a8b09dc3b5e9fae71ee6
Aaron <abader@abader.cptc.wisc.edu>
2013-12-19 13:20:14 -0600

    Fix bugs in compute_bs.  Get circle testing working.

--- compute_bs.f90
+++ compute_bs.f90
@@ -1,4 +1,4 @@
-subroutine compute_bs(p, isaux, bmag)
+subroutine compute_bs(p, isaux, b)
       
       ! call this routine once for every point that is being followed
       ! use successive pairs of points in each coil to define a "current stick". 
@@ -19,16 +19,14 @@ use coil_module
 
 implicit none
     
-integer :: i, j
+integer :: i, j, k
 real, dimension(:), allocatable :: xcoil, ycoil, zcoil 
 real, dimension(:), allocatable :: xcoilshift, ycoilshift, zcoilshift
 integer :: arggood
 real :: ax, ay, az, bx, by, bz, cx, cy, cz
-real :: cxax, cxay, cxaz, magcxa, magc, magb
-real :: adotc, adotb, cxasq, csq, bsq, f2c
-real :: sumcxf, sumcyf, sumczf, current
+real :: cxax, cxay, cxaz, magc, current
 integer :: coilnumber, numcoilpts, isaux
-real, dimension(3) :: p
+real, dimension(3) :: p, b, bseg
 real, dimension(1) :: blocal, bmag
 real :: mu0, pi
 
@@ -41,11 +39,15 @@ else
    coilnumber = main_count
 endif
 
+
 mu0=1.25663706E-6 ! in mks
 pi=3.14159265359
+!for debugging
+!mu0 = 1.0
 
 !loop for each coil
 do i=1,coilnumber
+
       
       if (isaux == 1) then
          numcoilpts = coil_set%aux_points(i)
@@ -55,8 +57,16 @@ do i=1,coilnumber
          current = coil_set%main_current(i)
       endif
 
+      ! make sure we actually have a coil
+      if (numcoilpts.le.1) then
+
+         cycle
+      endif
+      
+
 !     don't waste time with calculation if there's no current in this coil!
       if (current == 0) then
+
          cycle
       endif
    
@@ -66,93 +76,89 @@ do i=1,coilnumber
       allocate(xcoilshift(numcoilpts))
       allocate(ycoilshift(numcoilpts))
       allocate(zcoilshift(numcoilpts))
+
+
+      ! assign values to x, y, z arrays from the coil_module
+      if (isaux == 1) then
+        xcoil=coil_set%aux(i,1:numcoilpts,1)
+        ycoil=coil_set%aux(i,1:numcoilpts,2)
+        zcoil=coil_set%aux(i,1:numcoilpts,3)
+      else
+        xcoil=coil_set%main(i,1:numcoilpts,1)
+        ycoil=coil_set%main(i,1:numcoilpts,2)
+        zcoil=coil_set%main(i,1:numcoilpts,3)
+      endif
+
+      ! circularly shift coil point before the loop
+      ! this step can be pre-calculated to save time, 
+      ! probably doesn't save much though (AB)
+      xcoilshift=cshift(xcoil,1)
+      ycoilshift=cshift(ycoil,1)
+      zcoilshift=cshift(zcoil,1)
+      b = 0
+ 
             
       !loop over all points for each coil
-      do j=1,numcoilpts
-      
-      	! assign values to x, y, z arrays from the coil_module
-        if (isaux == 1) then
-          xcoil(j)=coil_set%aux(i,j,1)
-          ycoil(j)=coil_set%aux(i,j,2)
-          zcoil(j)=coil_set%aux(i,j,3)
-      	else
-          xcoil(j)=coil_set%main(i,j,1)
-          ycoil(j)=coil_set%main(i,j,2)
-          zcoil(j)=coil_set%main(i,j,3)
-        endif
+      do j=1,numcoilpts-1
+        bseg = 0 
+       
+        ! subtract point of interest r vector from coil points           
+        bx=xcoil(j)-p(1)
+        by=ycoil(j)-p(2)
+        bz=zcoil(j)-p(3)        
 
-      	! circularly shift coil point before the loop      
-      	xcoilshift=cshift(xcoil,1)
-      	ycoilshift=cshift(ycoil,1)
-      	zcoilshift=cshift(zcoil,1)
+        ! subtract point of interest r vector from shifted coil points
+        cx=xcoilshift(j)-p(1)
+        cy=ycoilshift(j)-p(2)
+        cz=zcoilshift(j)-p(3)
 
-            ! subtract point of interest r vector from coil points           
-            bx=xcoil(j)-p(1)
-            by=ycoil(j)-p(2)
-            bz=zcoil(j)-p(3)        
-                        
-            ! subtract point of interest r vector from shifted coil points
-            cx=xcoilshift(j)-p(1)
-            cy=ycoilshift(j)-p(2)
-            cz=zcoilshift(j)-p(3)
-                  
-            ! now, subtract the unshifted difference from the shifted difference
-            ax=cx-bx
-            ay=cy-by
-            az=cz-bz
-            
-            ! pull out NaN values, not sure why they're being created right now
-            ! looks like Paul does a version of the same thing
-            arggood = 1
-            if (ax/=ax .or. ay/=ay .or. az/=az) then
-            	arggood=0
-            end if
-            
-            ! use arggood to reset the loop if the current point is a NaN
-            if (arggood==0) then
-            	cycle
-            end if
+        ! now, subtract the unshifted difference from the shifted difference
+        ax=cx-bx
+        ay=cy-by
+        az=cz-bz
 
-            ! find cross products
-            cxax=cy*az-cz*ay
-            cxay=cz*ax-cx*az
-            cxaz=cx*ay-cy*ax
+        ! pull out NaN values, not sure why they're being created right now
+        ! looks like Paul does a version of the same thing
+        ! There shouldn't be any Nan values.  We need to track this down (AB)
+        arggood = 1
+        if (ax/=ax .or. ay/=ay .or. az/=az) then
+           arggood=0
+        end if
 
-            ! find dot products
-            adotc=ax*cx+ay*cy+az*cz
-            adotb=ax*bx+ay*by+az*bz
-            
-            ! find other, intermediate parts of the expression for b
-            cxasq=cxax*cxax+ cxay*cxay+ cxaz*cxaz
-            magcxa=sqrt(cxasq)
-            csq=cx*cx+cy*cy+cz*cz
-            magc=sqrt(cxasq)
-            bsq=bx*bx+by*by+bz*bz
-            magb=sqrt(bsq)
-            f2c=(adotc/magc-adotb/magb)/(cxasq*((4*pi)**2))
-                  
-            ! find intermediate sums
-            sumcxf=sumcxf+cxax*f2c
-            sumcyf=sumcyf+cxay*f2c
-            sumczf=sumczf+cxaz*f2c
-                  
-            ! find b for the coil
-            blocal=blocal + mu0*(current)*(sumcxf*sumcyf*sumczf)
-         
-            end do
-            
-	! add all values of b for each coil
-      bmag=bmag+blocal
-      
-      deallocate(xcoil)
-      deallocate(ycoil)
-      deallocate(zcoil)
-      deallocate(xcoilshift)
-      deallocate(ycoilshift)
-      deallocate(zcoilshift)
+        ! use arggood to reset the loop if the current point is a NaN
+        if (arggood==0) then
+           cycle
+        end if
+
+        ! find cross products
+        cxax=cy*az-cz*ay
+        cxay=cz*ax-cx*az
+        cxaz=cx*ay-cy*ax
+
+        ! pick up by aaron
+        magc = ((cx*cx) + (cy*cy) + (cz*cz))**1.5
+        bseg(1) = cxax
+        bseg(2) = cxay
+        bseg(3) = cxaz
+        bseg = bseg*mu0*current/4/pi/magc
+
+        do k=1,3
+           b(k) = b(k) + bseg(k)
+        enddo
+     enddo
+
+     deallocate(xcoil)
+     deallocate(ycoil)
+     deallocate(zcoil)
+     deallocate(xcoilshift)
+     deallocate(ycoilshift)
+     deallocate(zcoilshift)
+
+
+   
       
 end do  
        
-print *, bmag
+
 
 end subroutine compute_bs
--- test_bs.f90
+++ test_bs.f90
@@ -1,16 +1,56 @@
 program test_bs
   use coil_module
   implicit none
-  real, dimension(1) :: b
+  real, dimension(3) :: b
   integer :: i
 
-  call read_coils()
+  call make_simple_coil()
+
+!  call read_coils()
 
 ! set current
-  do i = 1,main_count
-     coil_set%main_current(i) = -150105./14
+!  do i = 1,main_count
+!     coil_set%main_current(i) = -150105./14
+!  enddo
+
+  call compute_bs((/1.0,0.,0./), 0, b)
+  print *,b
+
+end program test_bs
+
+
+! make a circular coil in the x-z plane centered at (1,0,0) with radius
+! of 0.5
+subroutine make_simple_coil()
+  use coil_module
+  implicit none
+
+  integer :: i, coil_points
+  real :: theta, pi, center, radius, mu0
+
+  mu0 = 1.25663706E-6
+  pi = 3.14159
+  coil_points = 201
+  radius = 0.5
+  center = 1
+
+  do i=0,coil_points-1
+     theta = 2*pi*i/(coil_points-1)
+     coil_set%main(1,i+1,1) = center + radius*cos(theta)
+     coil_set%main(1,i+1,3) = radius*sin(theta)
+!     print *,theta, cos(theta), coil_set%main(1,i,1)
   enddo
 
-  call compute_bs((/1.45,0.,0./), 0, b)
-  print *,b
-end program test_bs
+  coil_set%main_points(1) = coil_points
+  coil_set%main_current(1) = 1.0/mu0
+
+
+  do i=2,main_count
+     coil_set%main_points(i) = 0
+     coil_set%main_current(i) = 0.0
+
+  enddo
+
+
+end subroutine make_simple_coil
+  
