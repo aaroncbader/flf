9b1ec4d3f12d15c97f6779f28ac03b5ce7187921
Aaron <abader@abader.cptc.wisc.edu>
2014-01-09 12:27:43 -0600

    add a linear interpolation function to utility

--- utility.f90
+++ utility.f90
@@ -29,3 +29,48 @@ subroutine move_to_first_quad(r,z,phi,newr,newz,newphi)
   newr = r
 
 end subroutine move_to_first_quad
+
+! Given a function f(x) = y, find the value f(x0) by interpolation
+! x must be in increasing order, but need not be evenly spaced
+
+! numpoints is the size of the array
+real function linear_interpolate(x0, y, x, numpoints) 
+
+  implicit none
+  integer :: i,numpoints
+  real, dimension(numpoints) :: y, x
+  real :: x1, x2, y1, y2, x0
+
+  ! Handle out of bounds on the low end  
+  if (x0 .le. x(1)) then
+     x1 = x(1)
+     x2 = x(2)
+     y1 = y(1)
+     y2 = y(2)
+     print *,'test1'
+  else if (x0 .ge. x(numpoints)) then
+     x1 = x(numpoints - 1)
+     x2 = x(numpoints)
+     y1 = y(numpoints -1)
+     y2 = y(numpoints)
+     print *,'test2'
+  else
+     do i = 2,numpoints
+        if (x0 .le. x(i)) then
+           x1 = x(i-1)
+           x2 = x(i)
+           y1 = y(i-1)
+           y2 = y(i)
+           print *,x1,x2,y1,y2
+           exit
+        end if
+        if (i .eq. numpoints) then
+           !something bad happened here, this should not be possible
+           print *,'Error in linear interpolation'
+        end if
+     end do
+  end if
+
+  linear_interpolate = (y2 - y1) * (x0 - x1)/(x2 - x1) + y1
+
+end function linear_interpolate
