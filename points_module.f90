61aed01944dec9ab4b39f0789265a1863fe8ac6d
Laurie_vm <laurie@laurie-vm.(none)>
2014-01-15 15:18:50 -0600

    added extra bookeeping for divertor and limiter

---
+++ points_module.f90
@@ -0,0 +1,12 @@
+! This module will define allocatable arrays for use in
+! bookkeeping for points
+module points_module
+
+  implicit none
+  real, dimension(:,:), allocatable :: points_start, points_move, points_end
+  integer, dimension(:), allocatable :: points_hit_vessel, points_hit_divertor, points_hit_limiter
+  real :: points_dphi
+  integer :: n_iter, points_number
+  integer :: current_point
+
+end module points_module
