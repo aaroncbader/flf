52f3b5709116c5a93243d52abc9d9474ee114b02
Laurie_vm <laurie@laurie-vm.(none)>
2014-01-15 14:43:59 -0600

    created limiter module in same way as vessel module

---
+++ limiter_module.f90
@@ -0,0 +1,9 @@
+module limiter_module
+
+  implicit none
+  integer, dimension(2) :: limiter_size
+  
+  real, allocatable, dimension(:, :) :: limiter
+  character*144 :: limiter_file
+
+end module limiter_module
