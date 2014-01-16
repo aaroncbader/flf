0837923771026baab09082a56ad266ddb12dffcc
Laurie_vm <laurie@laurie-vm.(none)>
2014-01-16 14:40:46 -0600

    fixed dimensions of limiter array

--- limiter_module.f90
+++ limiter_module.f90
@@ -3,7 +3,7 @@ module limiter_module
   implicit none
   integer, dimension(2) :: limiter_size
   
-  real, allocatable, dimension(:, :) :: limiter
+  real, allocatable, dimension(:,:) :: limiter
   character*144 :: limiter_file
 
 end module limiter_module
