0992e54511c8b793216526c44e2620f74bcbb862
Laurie_vm <laurie@laurie-vm.(none)>
2014-01-22 09:30:36 -0600

    fixed a few bugs, column-major mix-ups, error in calculating the norm, and reading in the point with the proper number of variables

--- inside_limiter.f90
+++ inside_limiter.f90
@@ -44,51 +44,88 @@ end subroutine load_limiter
 
 
 ! note, point is in r,z,phi
-integer function inside_limiter(point)
+integer function inside_limiter(r,z,phi)
 
 use limiter_module
 
 implicit none
 
-real :: Xpoint, Ypoint
-integer :: in_polygon, poly_size
-real, dimension(3) :: bvector, baxis
-real, dimension(5) :: Xpoly, Ypoly
-real, dimension(3) :: point, pointc, HC_out, HC_up, HC_up_norm
+real :: Xpoint, Ypoint, dist_plane, delta, r, z, phi
+integer :: in_polygon, poly_size, is_near_helical_plane
+real, dimension(3) :: bvector, baxis, dist_axis
+real, dimension(4) :: Xpoly, Ypoly
+real, dimension(3) :: point, pointc, HC_out, HC_up, HC_up_mag, HC_up_norm
 
-poly_size=size(limiter(:,1))
+! use pre-calculated values of the location of the magnetic axis in QHS and the B field vector at this location
 
-Xpoly=limiter(1,:)
-Ypoly=limiter(2,:)
-
-! convert point from rzphi to xyz
-  call pol2cart(point,pointc)
-
-! use the same pre-calculated values for the normal vector
 bvector=(/0.0,0.43765823,0.24171643/)
+baxis=(/1.4454,0.0,0.0/)
+delta=0.01
 
-! need to project point into 2D plane
-! we'll do this in the same way that Chris Clark does in his matlab scripts
-! the 2d HC plane is defined by an 'out' coordinate: the x axis
-! and an 'up' coordinate, defined by the y anc z coordinates together
+  point=(/r,z,phi/)
+  
+  ! print *, 'point in limiter=', point
 
-HC_out=(/1.0,0.0,0.0/)
+  call pol2cart(point,pointc)
+ 
 
-! compute the cross product between HC_out and bvector (the normal vector)
-HC_up(1)=(HC_out(2)*bvector(3)-HC_out(3)*bvector(2))
-HC_up(2)=(HC_out(3)*bvector(1)-HC_out(1)*bvector(3))
-HC_up(3)=(HC_out(1)*bvector(2)-HC_out(2)*bvector(1))
+! use the B field vector as the normal vector to the helical plane. Find the distance to this plane by using the dot product.
+dist_axis=pointc-baxis
+dist_plane=dot_product(dist_axis,bvector)
 
-! normalize HC_up
-HC_up_norm=HC_up/(sqrt(sum(HC_up*HC_up)))
+! print *, dist_plane
 
-! find the dot product 
-Xpoint= pointc(1) 
-Ypoint= dot_product(pointc,HC_up) 
+if (abs(dist_plane) > delta) then
+   is_near_helical_plane=0
+   inside_limiter=0
+   return
+else if (dist_plane <= delta) then
 
-! now we can work within the 2d helical plane
-! use in_polygon to see if the point hits the limiter	
-inside_limiter=in_polygon(Xpoint, Ypoint, Xpoly, Ypoly, poly_size)
+	! print *, 'performing limiter check'
+
+	poly_size=size(limiter(1,:))
+
+	Xpoly=limiter(1,:)
+	Ypoly=limiter(2,:)
+
+	! need to project point into 2D plane
+	! we'll do this in the same way that Chris Clark does in his matlab scripts
+	! the 2d HC plane is defined by an 'out' coordinate: the x axis
+	! and an 'up' coordinate, defined by the y anc z coordinates together
+
+	HC_out=(/1.0,0.0,0.0/)
+
+	! compute the cross product between HC_out and bvector (the normal vector)
+	HC_up(1)=(HC_out(2)*bvector(3)-HC_out(3)*bvector(2))
+	HC_up(2)=(HC_out(3)*bvector(1)-HC_out(1)*bvector(3))
+	HC_up(3)=(HC_out(1)*bvector(2)-HC_out(2)*bvector(1))
+
+	! normalize HC_up
+
+	HC_up_mag=sqrt(HC_up(1)**2 + HC_up(2)**2 + HC_up(3)**2)	
+	
+	HC_up_norm=HC_up/HC_up_mag
+
+	
+	! print *, HC_up_norm
+
+	! find the dot product 
+	Xpoint= pointc(1) 
+
+	Ypoint= dot_product(pointc,HC_up_norm) 
+
+
+	! now we can work within the 2d helical plane
+	! use in_polygon to see if the point hits the limiter	
+
+
+	inside_limiter=in_polygon(Xpoint, Ypoint, Xpoly, Ypoly, poly_size)
+	
+	! print *, 'inside_limiter=', inside_limiter
+
+end if
+
+
 
 end function inside_limiter
 
