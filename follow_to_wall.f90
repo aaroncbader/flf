12269b5236f1ced488fcf4bb47be3cee5a8337a0
Aaron <abader@abader.cptc.wisc.edu>
2014-01-23 13:38:03 -0600

    remove outdated comments

--- follow_to_wall.f90
+++ follow_to_wall.f90
@@ -33,18 +33,7 @@ program follow_to_wall
       
         ! check if the last move left us inside the vessel
         p = points_move(j,:)
-        !isin = inside_vessel(p(1),p(2),p(3),vessel,vessel_size)
-        !if (isin == 0) then
-           ! Set point to hit
-        !   points_hit(j) = 1
-        !   cycle        
-        !endif
         
-        ! for some reason i can't understand removing this
-        ! print statement causes the field line follower
-        ! to fail.  I need to track this down, but it's
-        ! hard to debug without a print statement!
-        ! print *,points_move(j,:)
         ! set the current point
         current_point = j
 
