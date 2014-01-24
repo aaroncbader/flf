3c5a6fe15dce617d28d9f614a650587e4c6b4d7f
Laurie_vm <laurie@laurie-vm.(none)>
2014-01-24 13:59:37 -0600

    added distance calculation between old and new points to keep track of connection length (cherry picked from commit 0ac8331cdf903ea5f7f1ccc0d39fe22f66de1959)

--- follow_field.f90
+++ follow_field.f90
@@ -1,7 +1,7 @@
 ! p is the point in rzphi
 ! dphi is the distance in phi to follow
 
-subroutine follow_field(p, dphi)
+subroutine follow_field(p, dphi, dist)
 
   use coil_module
 
@@ -10,7 +10,7 @@ subroutine follow_field(p, dphi)
   parameter (liw=22)
   parameter (itask=1)
 
-  real, dimension(3) :: p
+  real, dimension(3) :: p, pold, pnew, poldc, pnewc
   real, dimension(2) :: y
   ! For now this is unused.
   real :: dphi, t0, t1, tol
@@ -18,6 +18,7 @@ subroutine follow_field(p, dphi)
   integer :: ifail, istate
   real, dimension(liw) :: iwork
   real, dimension(lrw) :: rwork
+  real, dimension(1) :: dist, r1, z1, phi1, r2, z2, phi2, x1, x2, y1, y2
 
   external field_deriv
 
@@ -27,6 +28,12 @@ subroutine follow_field(p, dphi)
   ! start and stop phi values
   t0 = p(3)
   t1 = p(3) + dphi
+  
+  ! keep track of old point
+  r1=y(1)
+  z1=y(2)
+  phi1=t0
+  pold=(/r1, z1, phi1/)
 
   tol = 1.e-8
 
@@ -39,6 +46,31 @@ subroutine follow_field(p, dphi)
   p(1) = y(1)
   p(2) = y(2)
   p(3) = t1
-
+  
+  ! keep track of new point
+  r2=p(1)
+  z2=p(2)
+  phi2=p(3)
+  pnew=(/r2,z2,phi2/)
+  
+  ! calculate distance between original point and new point
+  ! accumulate distance and call connection length
+  
+  ! first, convert to cartesian so we can use the distance formula
+  call pol2cart(pold,poldc)
+  call pol2cart(pnew,pnewc)
+  
+  x1=poldc(1)
+  y1=poldc(2)
+  
+  x2=pnewc(1)
+  y2=pnewc(2)
+  
+  dist=sqrt((x1-x2)**2+(y1-y2)**2+(z1-z2)**2)
+  
+  ! print *, dist
+  
+  
+  
   return
 end subroutine follow_field
