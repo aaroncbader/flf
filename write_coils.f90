dc84ae912a8de05cfb3d30deeda955ef1a252da1
Aaron Bader <abader@abader.cptc.wisc.edu>
2017-07-20 12:56:08 -0500

    fix aux coil writing

--- write_coils.f90
+++ write_coils.f90
@@ -17,7 +17,7 @@ subroutine write_coils
              coil_main(i,j,3)
      enddo
   enddo
-  do i=1,num_aux_coils
+  do i=1,aux_count
      print *,i,aux_points(i)
      write(40,*) aux_points(i), aux_current(i)
      do j=1,aux_points(i)
